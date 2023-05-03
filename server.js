1f0e54c68b4fd4253fb47db376b898e08f604a5e
unknown <nguyennhumay901@gmail.com>
2023-05-03 14:09:36 +0700

    UnCaught Exception and Unhandle Rejection handling

--- server.js
+++ server.js
@@ -4,16 +4,48 @@ const app = require("./app");
 
 dotenv.config({ path: "./config.env" });
 
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
+const connectDB = async () => {
+  try {
+    const DB = process.env.DATABASE.replace(
+      "<PASSWORD>",
+      process.env.DATABASE_PASSWORD
+    );
+    await mongoose.connect(DB);
+    console.log("connection successful");
+  } catch {
+    console.log("Database connection failed");
+    process.exit(1);
+  }
+};
 
-mongoose.connect(DB).then(() => {
-  console.log("connection successful");
-});
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+  const server = app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+  });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+  // Handle unhandle rejection error
+  process.on("unhandledRejection", (err) => {
+    console.log(err.name, err.message);
+    console.log("Unhandle Rejection! ... Server is shutting down...");
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+
+  // Handle uncaught exception error
+  process.on("uncaughtException", (err) => {
+    console.log(err.name, err.message);
+    console.log("Uncaught Exception! ... Server is shutting down...");
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+};
+
+const initializeApp = async () => {
+  await connectDB();
+  startServer();
+};
+
+initializeApp();
